807239410b918c8119cc2f46bbb197cdbdbd57f3
joseluis gomez <joseluisgomezcecegna@gmail.com>
2024-09-17 00:47:21 -0700

    Added Auth with Breeze.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
 
 /*
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
@@ -19,3 +25,20 @@ Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.
 
 Route::resource('posts', PostController::class);
 Route::get('/posts/{post}/delete', [PostController::class, 'delete'])->name('posts.delete');//custom route for delete confirmation.
+
+
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+
+
+
